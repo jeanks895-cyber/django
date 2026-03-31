f9cb84222c4b28d0fdd564630ed6919ea753a5f2
Jean <jeanks895@gmail.com>
2026-03-31 12:54:39 +0530

    Add initial Django project structure and configuration files
    - Created manage.py for administrative tasks.
    - Added __init__.py to myproject directory.
    - Generated settings.py with default configurations.
    - Created urls.py for URL routing.
    - Added wsgi.py and asgi.py for WSGI and ASGI configurations.
    - Included necessary middleware and installed apps in settings.py.
    - Set up database configuration using SQLite.
    - Established URL patterns in urls.py to include admin and customers app.

--- cw8/myapp/apps.py
+++ cw8/myapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class LoginappConfig(AppConfig):
+class MyappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'loginapp'
+    name = 'myapp'
---
+++ cw8/myapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'myapp/home.html')
+
+def about(request):
+    return render(request, 'myapp/about.html')
